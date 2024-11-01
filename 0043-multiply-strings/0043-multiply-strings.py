e101e8f51b7640d7056ed6c72611fd3a921bc9aa
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-11-01 18:15:59 +0300

    Time: 0 ms (100%), Space: 16.6 MB (35.89%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,11 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        num3 = 0
+        num4 = 0
+        for i in num1:
+            num3 = num3 * 10
+            num3 = num3 + ord(i) - 48
+        for j in num2:
+            num4 = num4 * 10
+            num4 = num4 + ord(j) - 48
+        return f"{num3*num4}"
