807f2321c97f9fe8ffc535e1271db907c3484776
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-11-14 13:02:16 +0300

    Time: 10 ms (25.46%), Space: 17.9 MB (62.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,8 @@ class Solution:
         zero = 0
         number = 0
         while number < len(nums):
-            if nums[zero] == 0 and nums[number] != 0:
+            if nums[number] != 0:
                 nums[zero], nums[number] =  nums[number], nums[zero]
                 zero += 1
-                number += 1
-            elif nums[zero] == 0 and nums[number] == 0:
-                number += 1
-            else:
-                zero += 1
-                number += 1
+            number += 1
         return nums           
