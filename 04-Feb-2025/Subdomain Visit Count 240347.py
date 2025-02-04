ec4b2f1726316a3b7bb5ba9812cc5392784ee012
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-04 17:23:55 +0300

    Created 'Subdomain Visit Count' by A2SV Hub

---
+++ 04-Feb-2025/Subdomain Visit Count 240347.py
@@ -0,0 +1,18 @@
+# Problem: Subdomain Visit Count - https://leetcode.com/problems/subdomain-visit-count
+
+from collections import defaultdict
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        dic = defaultdict(int)
+        res = []
+        for i in cpdomains:
+            num, domain = i.split()
+            num = int(num)
+            arr = domain.split(".")
+            test = ""
+            for j in range(len(arr)):
+                test = ".".join(arr[j:len(arr)+1])
+                dic[test] += num
+        for k in dic:
+            res.append(" ".join([str(dic[k]), k]))
+        return res
