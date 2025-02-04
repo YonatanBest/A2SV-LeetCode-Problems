36c42f4ac4497fd6c033348fc57cbd9dfd1c6232
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-04 15:26:11 +0300

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 04-Feb-2025/Contains Duplicate 239874.py
@@ -0,0 +1,11 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arr = set()
+        for i in nums:
+            if i in arr:
+                return True
+            else:
+                arr.add(i)
+        return False
