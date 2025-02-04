170f0fc87a2211f525b336d9ce4a355a21a7324d
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-04 15:36:55 +0300

    Time: 6 ms (61.64%), Space: 19.8 MB (16.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,6 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums) == 2:
-            return nums[0]
         majority = ceil(len(nums)/2)
         rep = Counter(nums)
         for i in rep:
