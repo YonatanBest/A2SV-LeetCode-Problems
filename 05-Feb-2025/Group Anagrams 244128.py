91655a16180cbec9096c43bc0a9dcd1d2402969c
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-05 18:17:39 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 05-Feb-2025/Group Anagrams 244128.py
@@ -0,0 +1,15 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        for i in strs:
+            x = i
+            if "".join(sorted(list(x))) in dic:
+                dic["".join(sorted(list(x)))].append(i)
+            else:
+                dic["".join(sorted(list(x)))] = [i]
+        arr = []
+        for j in dic:
+            arr.append(dic[j])
+        return arr
