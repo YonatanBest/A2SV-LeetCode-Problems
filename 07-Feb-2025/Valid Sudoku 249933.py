7d3f7ff92fca31f9fe018a8c71c1755fef934284
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-07 16:22:31 +0300

    Created 'Valid Sudoku' by A2SV Hub

---
+++ 07-Feb-2025/Valid Sudoku 249933.py
@@ -0,0 +1,19 @@
+# Problem: Valid Sudoku - https://leetcode.com/problems/valid-sudoku/
+
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        box = defaultdict(set)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in box[i//3, j//3] or board[i][j] in row[i] or board[i][j] in col[j]:
+                    return False
+                else:
+                    box[i//3, j//3].add(board[i][j])
+                    row[i].add(board[i][j])
+                    col[j].add(board[i][j])
+        return True
