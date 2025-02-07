0827990595ce54408d0f89cf0d2825c36033f787
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-07 15:17:07 +0300

    Created 'Transpose Matrix' by A2SV Hub

---
+++ 07-Feb-2025/Transpose Matrix 249701.py
@@ -0,0 +1,9 @@
+# Problem: Transpose Matrix - https://leetcode.com/problems/transpose-matrix/
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        arr = [[0 for i in range(len(matrix))] for i in range(len(matrix[-1]))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                arr[j][i] = matrix[i][j]
+        return arr
