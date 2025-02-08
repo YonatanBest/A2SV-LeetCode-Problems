10fb6ab3136a61d0c0ea11c0af6bbffe151d6da0
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-08 17:04:46 +0300

    Created 'Transformed Array' by A2SV Hub

---
+++ 08-Feb-2025/Transformed Array 251458.py
@@ -0,0 +1,9 @@
+# Problem: Transformed Array - https://leetcode.com/problems/transformed-array/description/
+
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        result = [0 for i in range(len(nums))]
+        for i in range(len(nums)):
+            index = (nums[i] + i) % len(nums)
+            result[i] = nums[index]
+        return result
