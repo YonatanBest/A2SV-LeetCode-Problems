c1e15235a891290bfcff718bc6258b0e876c9a53
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-09 14:36:03 +0300

    Created 'Spiral Matrix' by A2SV Hub

---
+++ 09-Feb-2025/Spiral Matrix 251994.py
@@ -0,0 +1,37 @@
+# Problem: Spiral Matrix - https://leetcode.com/problems/spiral-matrix/
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        dir = 0
+        matrix_arr = []
+        row = 0
+        col = 0
+        for i in range(len(matrix) * len(matrix[0])):
+            matrix_arr.append(matrix[row][col])
+            temp = [row, col]
+            if dir == 0:
+                if col + 1 >= len(matrix[0]) or matrix[row][col + 1] == None:
+                    dir += 1
+                    row += 1
+                else:
+                    col += 1
+            elif dir == 1:
+                if row + 1 >= len(matrix) or matrix[row + 1][col] == None:
+                    dir += 1
+                    col -= 1
+                else:
+                    row += 1
+            elif dir == 2:
+                if col - 1 < 0 or matrix[row][col - 1] == None:
+                    dir += 1
+                    row -= 1
+                else:
+                    col -= 1
+            else:
+                if row - 1 < 0 or matrix[row - 1][col] == None:
+                    dir = 0
+                    col += 1 
+                else:
+                    row -= 1
+            matrix[temp[0]][temp[1]] = None
+        return matrix_arr
