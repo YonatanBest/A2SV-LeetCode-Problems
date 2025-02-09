0f191ea2cd7f0a1736ee84a19fac2c74fba578cf
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-09 10:38:31 +0300

    Time: 0 ms (100%), Space: 17.8 MB (36.43%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.py
+++ 0861-flipping-an-image/0861-flipping-an-image.py
@@ -5,9 +5,10 @@ class Solution:
             j = len(image[row]) - 1
             while i < j:
                 image[row][i], image[row][j] = image[row][j], image[row][i]
+                image[row][i]^= 1
+                image[row][j]^= 1
                 i += 1
                 j -= 1
-        for row in range(len(image)):
-            for col in range(len(image)):
-                image[row][col] =  1 - image[row][col]
+            if len(image[row]) % 2:
+                image[row][j]^= 1
         return image
