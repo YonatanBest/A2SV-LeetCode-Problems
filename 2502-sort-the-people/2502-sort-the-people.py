24e0d4446abf1d8c0a35aeaf29275f709367331b
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-10 15:01:14 +0300

    Time: 435 ms (7.74%), Space: 18.3 MB (41.17%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -4,13 +4,15 @@ class Solution:
         for i in range(len(names)):
             dic[heights[i]] = names[i]
         for times in range(len(heights)):
-            check = True
-            for i in range(1, len(heights) - times):
-                if heights[i - 1] < heights[i]:
-                    heights[i - 1], heights[i] = heights[i], heights[i - 1]
-                    check = False
-            if check:
-                break
+            min_curr = times
+            i = times + 1
+            while i < len(heights):
+                if heights[i] > heights[min_curr]:
+                    min_curr = i
+                i += 1
+            heights[times], heights[min_curr] = heights[min_curr], heights[times]
+
+
         names = []
         for j in heights:
             names.append(dic[j])
