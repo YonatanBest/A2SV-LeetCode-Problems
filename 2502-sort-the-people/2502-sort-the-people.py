7d257303372de0dc4b8e63433ece851add9588f0
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-10 14:40:06 +0300

    Time: 771 ms (5.03%), Space: 18.2 MB (41.17%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        dic = {}
+        for i in range(len(names)):
+            dic[heights[i]] = names[i]
+        for times in range(len(heights)):
+            check = True
+            for i in range(1, len(heights)):
+                if heights[i - 1] < heights[i]:
+                    heights[i - 1], heights[i] = heights[i], heights[i - 1]
+                    check = False
+            if check:
+                break
+        names = []
+        for j in heights:
+            names.append(dic[j])
+        return names
