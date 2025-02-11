f9a3310fbc165af39f7f2e30c5395e6ca79628a6
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-11 14:58:46 +0300

    Time: 0 ms (100%), Space: 22.7 MB (48.7%) - LeetHub

---
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        dic = {}
+        arr = []
+        for i in range(len(score)):
+            arr.append(score[i][k])
+            dic[score[i][k]] = i
+        arr.sort(reverse=True)
+        for j in range(len(arr)):
+            dic[score[j][k]] = dic[arr[j]]
+            score[j], score[dic[arr[j]]] = score[dic[arr[j]]], score[j]
+        return score
