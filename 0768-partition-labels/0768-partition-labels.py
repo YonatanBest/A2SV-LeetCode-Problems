711f8a9e5c61aa2657582c883aeaa41ece5b3ba5
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-12 16:50:51 +0300

    Time: 3 ms (90.36%), Space: 17.9 MB (13.84%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        dic = {}
+        for i in range(len(s)):
+            if s[i] in dic:
+                dic[s[i]].add(i)
+            else:
+                dic[s[i]] = set([i])
+        arr = []
+        for i in dic:
+            arr.append([min(dic[i]), max(dic[i])])
+        arr.sort()
+        for i in range(1, len(arr)):
+            if arr[i][0] < arr[i - 1][1]:
+                arr[i] = [arr[i - 1][0], max(arr[i][1], arr[i - 1][1])]
+                arr[i - 1] = None
+            else:
+                pass
+        ans = []
+        for i in arr:
+            if i != None:
+                ans.append((i[1] - i[0]) + 1)
+        return ans
