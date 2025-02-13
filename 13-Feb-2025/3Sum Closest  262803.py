631f7efb1b81d98c764b8fdffc2e38b466c4a914
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-13 15:49:34 +0300

    Created '3Sum Closest ' by A2SV Hub

---
+++ 13-Feb-2025/3Sum Closest  262803.py
@@ -0,0 +1,23 @@
+# Problem: 3Sum Closest  - https://leetcode.com/problems/3sum-closest/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        k = len(nums) - 1
+        closest_sum = sum(nums[:3])
+        while k >= 2:
+            i = 0
+            j = k - 1
+            while i < j:
+                curr_sum = nums[i] + nums[j] + nums[k]
+                if curr_sum == target:
+                    return curr_sum
+                elif curr_sum < target:
+                    i += 1
+                else:
+                    j -= 1
+                if abs(closest_sum - target) > abs(curr_sum - target):
+                    closest_sum = curr_sum
+            k -= 1
+
+        return closest_sum
