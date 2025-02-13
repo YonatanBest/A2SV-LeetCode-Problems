493d93d771df0be6d7a680f8f3b7a43f8e63d6c2
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-13 11:07:13 +0300

    Time: 19 ms (57.19%), Space: 17.7 MB (78.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,17 +3,15 @@ class Solution:
         max_substring = 0
         i = 0
         j = 0
-        dic = {}
+        dic = set()
         while j < len(s):
             if s[j] not in dic:
-                dic[s[j]] = 1
+                dic.add(s[j])
             else:
                 while s[j] in dic:
-                    dic[s[i]] -= 1
-                    if dic[s[i]] == 0:
-                        del dic[s[i]]
+                    dic.remove(s[i])
                     i += 1
-                dic[s[j]] = 1
+                dic.add(s[j])
             max_substring = max(max_substring, len(dic))
             j += 1
         return max_substring
