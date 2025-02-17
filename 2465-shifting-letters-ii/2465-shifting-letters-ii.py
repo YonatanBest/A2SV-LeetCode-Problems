e2f7ad40aac986041dcacc05e510fce0b2ad434b
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-17 15:22:18 +0300

    Time: 64 ms (41.86%), Space: 41.3 MB (86.44%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:        
+        arr = [0]*len(s)
+
+        for left, right, k in shifts:
+            if k == 1:
+                arr[left] += 1
+                if right + 1 < len(arr):
+                    arr[right+1] -=1
+            else:
+                arr[left] -= 1
+                if right + 1 < len(arr):
+                    arr[right+1] +=1
+
+        for i in range(1, len(s)):
+            arr[i] += arr[i - 1]
+
+        for j in range(len(s)):
+            arr[j] += ord(s[j]) - 97
+
+        for x in range(len(arr)):
+            arr[x] = chr((arr[x] % 26) + 97)
+        
+        return "".join(arr)
