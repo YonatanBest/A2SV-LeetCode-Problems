21954550b92b0daf1f364703d971ea8ca4321757
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-18 15:38:38 +0300

    Time: 27 ms (84.97%), Space: 18.5 MB (99.15%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,26 +1,20 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         n = len(nums)
-        ones = [0] * len(nums)
-        one = n
-        for i in range(n - 1, -1, -1):
-            if nums[i] == 1:
-                one = i
+        def atmost(nums, goal):
+            left = answer = total = 0
+            n = len(nums)
+            for right in range(n):
+                total += nums[right]
+                while total > goal:
+                    total -= nums[left]
+                    left += 1
+                
+                answer += right - left + 1
+            
+            return answer
 
-            ones[i] = one
-        
-        left = 0
-        ways = 0
-        total = 0
-        for right in range(len(nums)):
-            total += nums[right]
-            while total > goal:
-                total -= nums[left]
-                left += 1
+        if goal:
+            return atmost(nums, goal) - atmost(nums, goal - 1)
 
-            if total == goal:
-                if goal:
-                    ways += ones[left] - left + 1
-                else:
-                    ways += right - left + 1
-        return ways
+        return atmost(nums, goal)
