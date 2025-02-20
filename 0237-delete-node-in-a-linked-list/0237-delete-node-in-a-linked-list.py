e3bede9956ed5b0f7ca4d7e328fd4103375b3be8
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-02-20 10:33:19 +0300

    Time: 51 ms (6.95%), Space: 18.2 MB (5.43%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -10,9 +10,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        current = node
-        while current and current.next:
-            temp = current
-            current.val = current.next.val
-            current = current.next
-        temp.next = None
+        node.val = node.next.val
+        node.next = node.next.next
