3d6e7b347da9b1f9bd22068727186017fd5a3765
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-04 12:30:41 +0300

    Created 'Two City Scheduling' by A2SV Hub

---
+++ 04-Mar-2025/Two City Scheduling 290795.py
@@ -0,0 +1,11 @@
+# Problem: Two City Scheduling - https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0] - x[1])
+        totalsum = 0
+        for i in range(0, len(costs)//2):
+            totalsum += costs[i][0]
+        for i in range(len(costs)//2, len(costs)):
+            totalsum += costs[i][1]
+        return totalsum
