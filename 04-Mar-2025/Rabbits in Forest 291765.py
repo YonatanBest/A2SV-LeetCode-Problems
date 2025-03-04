30b9499edb3dd7572e25917f4b2d5be2e9dc5a3a
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-04 18:56:04 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 04-Mar-2025/Rabbits in Forest 291765.py
@@ -0,0 +1,14 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        dic = Counter(answers)
+        count = 0
+        for i in dic:
+            temp = 0
+            temp += (i + 1)*(dic[i] // (i+1))
+            if dic[i] % (i + 1):
+                temp += i + 1
+            count += temp
+        print(count)
+        return count
