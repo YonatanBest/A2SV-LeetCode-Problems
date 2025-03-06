15533f5329d487e0e4fe4cd45a0f971242a2f0fd
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-06 17:41:16 +0300

    Created 'Next Greater Element' by A2SV Hub

---
+++ 06-Mar-2025/Next Greater Element 294598.py
@@ -0,0 +1,17 @@
+# Problem: Next Greater Element - https://leetcode.com/problems/next-greater-element-i/
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = {}
+        test = []
+        for i in nums2:
+            while test and test[-1] < i:
+                ans[test.pop()] = i
+            test.append(i)
+        test = []
+        for i in nums1:
+            if i in ans:
+                test.append(ans[i])
+            else:
+                test.append(-1)
+        return test
