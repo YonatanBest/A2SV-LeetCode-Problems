34bfb56c09721b975b2eed1d4247d4d41ea4b531
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-20 20:03:15 +0300

    Time: 7188 ms (5.18%), Space: 24.9 MB (6.53%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -1,16 +1,28 @@
+import itertools
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         self.min_diff = float("inf")
-
-        def backtrack(path, i):
-            if len(cookies) == i:
-                self.min_diff = min(max(path), self.min_diff)
-                return
-            for j in range(k):
-                if path[j] + cookies[i] > self.min_diff:
-                    continue
-                path[j] += cookies[i]
-                backtrack(path, i + 1)
-                path[j] -= cookies[i]       
-        backtrack([0]*k, 0)
+        all = set()
+        for permutation in itertools.permutations(cookies):
+            all.add(permutation)
+        all = list(all)
+        self.total = sum(cookies)
+        for cook in all:
+            def backtrack(i, path):
+                if len(cookies) == i:
+                    temp = max(path)
+                    if len(path) == k and self.total == sum(path):
+                        self.min_diff = min(temp, self.min_diff)
+                    return
+                
+                num = 0
+                for j in range(i, len(cookies)):
+                    if num + cook[j] > self.min_diff:
+                        continue
+                    num = num + cook[j]
+                    path.append(num)
+                    backtrack(j + 1, path)
+                    path.pop()
+                return 
+            backtrack(0, [])
         return self.min_diff
