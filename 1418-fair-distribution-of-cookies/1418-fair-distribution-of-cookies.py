b197e672c0bae7fdf7db2ab17ce42d5a1ce66839
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-20 19:57:46 +0300

    Time: 3687 ms (18.92%), Space: 18 MB (32.66%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -3,12 +3,12 @@ class Solution:
         self.min_diff = float("inf")
 
         def backtrack(path, i):
-            if path and max(path) > self.min_diff and self.min_diff != float("inf"):
-                return
             if len(cookies) == i:
                 self.min_diff = min(max(path), self.min_diff)
                 return
             for j in range(k):
+                if path[j] + cookies[i] > self.min_diff:
+                    continue
                 path[j] += cookies[i]
                 backtrack(path, i + 1)
                 path[j] -= cookies[i]       
