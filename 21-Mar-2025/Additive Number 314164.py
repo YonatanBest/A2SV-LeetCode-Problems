7fadd30c908ff2463fe4b52c3eac8752cec8e4eb
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-21 13:58:56 +0300

    Created 'Additive Number' by A2SV Hub

---
+++ 21-Mar-2025/Additive Number 314164.py
@@ -0,0 +1,28 @@
+# Problem: Additive Number - https://leetcode.com/problems/additive-number/
+
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        if len(num) < 3:
+            return False
+        self.check = False
+        def backtrack(i, path):
+            if len(path) > 2 and int(path[len(path) - 1]) != int(path[len(path) - 2]) + int(path[len(path) - 3]):
+                return
+            if len(num) == i:
+                test = True
+                for k in range(2, len(path)):
+                    if int(path[k]) != int(path[k - 1]) + int(path[k - 2]):
+                        test = False
+                if test and len(path) > 2 and (len(path) != 3 or int(path[2]) == int(path[0]) + int(path[1])):
+                    self.check = True
+            nums = ""
+            for j in range(i, len(num)):
+                nums += num[j]
+                if len(nums) != 1 and nums[0] == "0":
+                    return
+                path.append(nums)
+                backtrack(j + 1, path)
+                path.pop()
+            return
+        backtrack(0, [])
+        return self.check
