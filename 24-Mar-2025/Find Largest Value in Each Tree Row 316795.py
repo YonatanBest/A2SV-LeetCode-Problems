8de1396cd005ba5de292fd3e7df430e3de10c624
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-24 17:26:35 +0300

    Created 'Find Largest Value in Each Tree Row' by A2SV Hub

---
+++ 24-Mar-2025/Find Largest Value in Each Tree Row 316795.py
@@ -0,0 +1,23 @@
+# Problem: Find Largest Value in Each Tree Row - https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.level = []
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        def finder(num, root):
+            if not root:
+                return
+            if len(self.level) <= num:
+                self.level.append(root.val)
+            else:
+                self.level[num] = max(root.val, self.level[num])
+            finder(num + 1, root.right)
+            finder(num + 1, root.left)
+        finder(0, root)
+        return self.level
