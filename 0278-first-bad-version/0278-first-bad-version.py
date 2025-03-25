9c1b6b8252cc541a6d0a9513305898ab7e47278c
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-25 19:35:50 +0300

    Time: 29 ms (94.39%), Space: 17.7 MB (44.34%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        i = 1
+        j = n
+        mid = (i + j)//2
+        while i <= j:
+            if isBadVersion(mid):
+                j = mid - 1
+            else:
+                i = mid + 1
+            mid = (i + j)//2
+        return j + 1
