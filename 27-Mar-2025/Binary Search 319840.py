5017206256a1bc5568bb67ecdba3b2f99fb35f18
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-27 17:53:03 +0300

    Created 'Binary Search' by A2SV Hub

---
+++ 27-Mar-2025/Binary Search 319840.py
@@ -0,0 +1,21 @@
+# Problem: Binary Search - https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        self.ans = -1
+        i = 0
+        j = len(nums) - 1
+        mid = (i + j)//2
+        def binarysearch(i, j, mid):
+            if i > j:
+                return -1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                j = mid - 1
+            else:
+                i = mid + 1
+            mid = (i + j)//2
+            return binarysearch(i, j, mid)
+        self.ans = binarysearch(i, j, mid)
+        return self.ans
