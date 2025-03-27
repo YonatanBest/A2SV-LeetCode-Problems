f0728a1e8d644ca43147efcd5c8567983ea7b38b
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-27 17:53:20 +0300

    Created 'First Bad Version' by A2SV Hub

---
+++ 27-Mar-2025/First Bad Version 319844.py
@@ -0,0 +1,17 @@
+# Problem: First Bad Version - https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        i = 1
+        j = n
+        mid = (i + j)//2
+        while i <= j:
+            if isBadVersion(mid):
+                j = mid - 1
+            else:
+                i = mid + 1
+            mid = (i + j)//2
+        return i
