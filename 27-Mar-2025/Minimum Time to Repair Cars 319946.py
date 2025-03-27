58e7b189623787cebf88a6a97cf455cbbc864832
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-03-27 18:24:40 +0300

    Created 'Minimum Time to Repair Cars' by A2SV Hub

---
+++ 27-Mar-2025/Minimum Time to Repair Cars 319946.py
@@ -0,0 +1,26 @@
+# Problem: Minimum Time to Repair Cars - https://leetcode.com/problems/minimum-time-to-repair-cars/
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        temp = None
+        i = 1
+        j = max(ranks)*(cars**2)
+        mid = (i + j)//2
+
+        def validate(mid):
+            carable = 0
+            for rank in ranks:
+                carable += int(sqrt(mid/rank))
+            if carable < cars:
+                return False
+            return True
+
+        while i <= j:
+            if validate(mid):
+                temp = mid
+                j = mid - 1
+            else:
+                i = mid + 1
+            mid = (i + j)//2
+        return temp
+        
