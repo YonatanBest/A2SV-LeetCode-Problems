896de86f7c704f95a60f5ade890e370bee6a16f5
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-04-01 18:49:39 +0300

    Time: 52 ms (88.3%), Space: 25.3 MB (50.23%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,33 +1,18 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge_sort(arr):
-            if len(arr) > 1:
-                left_arr = arr[:len(arr)//2]
-                right_arr = arr[len(arr)//2:]
-                
-                merge_sort(left_arr)
-                merge_sort(right_arr)
-
-                left = 0
-                right = 0
-                new_index = 0
-
-                while left < len(left_arr) and right < len(right_arr):
-                    if left_arr[left] > right_arr[right]:
-                        arr[new_index] = right_arr[right]
-                        right += 1
-                    else:
-                        arr[new_index] = left_arr[left]
-                        left += 1
-                    new_index += 1
-                while left < len(left_arr):
-                    arr[new_index] = left_arr[left]
-                    left += 1
-                    new_index += 1
-                while right < len(right_arr):
-                    arr[new_index] = right_arr[right]
-                    right += 1
-                    new_index += 1
-
-        merge_sort(nums)     
+        bucket = [[], [], [], [], [], [], [], [], [], [], []]
+        for i in nums:
+            idx = i//10_000 + 5
+            bucket[idx].append(i)
+            # right = len(bucket[idx]) - 2
+            # while right >= 0:
+            #     if bucket[idx][right] > bucket[idx][right + 1]:
+            #         bucket[idx][right], bucket[idx][right + 1] = bucket[idx][right + 1], bucket[idx][right]
+            #         right -= 1
+            #     else:
+            #         break
+        print(bucket)
+        nums = []
+        for arr in bucket:
+            nums += sorted(arr)
         return nums
