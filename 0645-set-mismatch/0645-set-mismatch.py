a98f637be64a358fafd89597be2f90eb11d2d999
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-04-04 21:11:39 +0300

    Time: 11 ms (69.92%), Space: 18.8 MB (97.54%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dic = defaultdict(int)
+        for num in nums:
+            if num in dic:
+                total = sum(nums) - num
+                lost = (len(nums)*(len(nums)+1))//2 - total
+                return [num, lost]
+            dic[num] += 1
+        return []
