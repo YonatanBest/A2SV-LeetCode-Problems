667b8b37916f56465ac95e1261e2245f986a8499
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-04-08 19:33:12 +0300

    Time: 172 ms (13.11%), Space: 21.1 MB (20.8%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,19 +1,22 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         dir = [(-1,0), (0, -1), (1, 0), (0, 1)]
-        total_row = len(grid)
-        total_col = len(grid[0])
         perimeter = 0
-        for i in range(total_row):
-            for j in range(total_col):
-                if grid[i][j]:
-                    for x, y in dir:
-                        if 0 <= i + x < total_row and 0 <= j + y < total_col:
-                            if not grid[i + x][j + y]:
-                                perimeter += 1
-                        else:
-                            perimeter += 1
-                    print(perimeter, i, j)
+        def inbound(r, c):
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+        def dfs(row, col, visited):
+            nonlocal perimeter
+            visited.add((row, col))
+            for row_cha, col_cha in dir:
+                new_row, new_col = row + row_cha, col + col_cha
+                if inbound(new_row, new_col) and grid[new_row][new_col]:
+                    if (new_row, new_col) not in visited:
+                        dfs(new_row, new_col, visited)
+                else:
+                    perimeter += 1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c]:
+                    dfs(r, c, set())  
+                    return perimeter
         return perimeter
-
-            
