9678f2d465cb2e70022fd6f07e569fe82a504b01
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-04-22 23:09:48 +0300

    Time: 0 ms (100%), Space: 17.8 MB (74.37%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,46 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = []
+        test = {}
+        
+        for i in range(len(equations)):
+            if equations[i][0] not in test:
+                test[equations[i][0]] = len(graph)
+                graph.append([[equations[i][1], values[i]]])
+            else:
+                graph[test[equations[i][0]]].append([equations[i][1], values[i]])
+                
+            if equations[i][1] not in test and values[i]:
+                test[equations[i][1]] = len(graph)
+                graph.append([[equations[i][0], 1/values[i]]])
+            elif values[i]:
+                graph[test[equations[i][1]]].append([equations[i][0], 1/values[i]])
+                
+        
+        def traverse(start,end, visited):
+            if start not in test:
+                return  -1 
+                
+            if end not in test:
+                return -1 
+                
+            visited.add(start)
+            
+            if start == end:
+                return 1
+            
+            for j in graph[test[start]]:   
+                
+                if j[0] not in visited:   
+                    temp = traverse(j[0],end,visited)
+                    
+                    if temp != -1:
+                        return j[1]*temp
+            return -1
+        
+        ans = []
+        for que in queries:
+            ans.append(traverse(que[0], que[1], set()))
+                
+            # print()
+        return ans
