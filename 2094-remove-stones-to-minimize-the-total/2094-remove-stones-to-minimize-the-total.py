2dcdca0df174082c0af171f0393fb8dfddf8b4e1
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-04-29 18:58:49 +0300

    Time: 519 ms (89.65%), Space: 29.3 MB (32.51%) - LeetHub

--- 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.py
@@ -1,23 +1,16 @@
+from heapq import heapify,heappop,heappush
+
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        piles.sort(reverse=True)
+        piles = [-pile for pile in piles]
+        heapify(piles) #O(n)
+    
         for j in range(k):
-            # print(piles)
-            piles[0] -= piles[0]//2
-            parent = 0
+            current_max = -heappop(piles)
+            new_element = current_max - current_max//2
             
-            while 2*parent + 2 < len(piles) and (piles[parent] < piles[2*parent + 1] or piles[parent] < piles[2*parent + 2]):
-                if piles[2*parent + 1] < piles[2*parent + 2]:
-                    piles[2*parent + 2], piles[parent] = piles[parent], piles[2*parent + 2]
-                    parent = 2*parent + 2
-                else:
-                    piles[2*parent + 1], piles[parent] = piles[parent], piles[2*parent + 1]
-                    parent = 2*parent + 1
+            heappush(piles,-new_element)
                     
-            while 2*parent + 1 < len(piles) and piles[parent] < piles[2*parent + 1]:
-                    piles[2*parent + 1], piles[parent] = piles[parent], piles[2*parent + 1]
-                    parent = 2*parent + 1
-                    
-        return sum(piles)
+        return -sum(piles)
             
         
