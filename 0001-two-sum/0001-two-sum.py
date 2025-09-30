5e65a0e9565bce7ea8ab11cf3113c0cb5b7f372a
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-09-30 23:42:04 +0300

    Time: 7 ms (36.67%), Space: 19.6 MB (5.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,23 @@
-from collections import Counter
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {}
+    def twoSum(self, nums: List[int], target: int):
+        dic = {}
         for i in range(len(nums)):
-            if nums[i] in nums_dict:
-                nums_dict[nums[i]].append(i)
+            if nums[i] in dic:
+                dic[nums[i]].append(i)
             else:
-                nums_dict[nums[i]] = [i]
+                dic[nums[i]] = [i]
         nums.sort()
         i = 0
         j = len(nums) - 1
         while i < j:
-            if nums[i] + nums[j] > target:
-                j -= 1
-            elif nums[i] + nums[j] < target:
-                i += 1
-            else:
+            if nums[i] + nums[j] == target:
                 if nums[i] == nums[j]:
-                    return [nums_dict[nums[i]][0], nums_dict[nums[i]][1]]
+                    return [dic[nums[i]][0], dic[nums[i]][1]]
                 else:
-                    return [nums_dict[nums[i]][0], nums_dict[nums[j]][0]]
+                    return [dic[nums[i]][0], dic[nums[j]][0]]
+            elif nums[i] + nums[j] > target:
+                j -= 1
+            else:
+                i += 1
         return -1
+            
