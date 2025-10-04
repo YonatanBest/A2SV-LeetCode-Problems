4352aab2259f25788d1fb37357ed0be4573a20ea
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-04 21:00:12 +0300

    Time: 0 ms (100%), Space: 17.8 MB (70.62%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,9 +1,20 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key=lambda x: x[0] - x[1])
-        totalsum = 0
-        for i in range(0, len(costs)//2):
-            totalsum += costs[i][0]
-        for i in range(len(costs)//2, len(costs)):
-            totalsum += costs[i][1]
-        return totalsum
+        costs.sort(key=lambda x: abs(x[0] - x[1]), reverse=True)
+        num_city_a = 0
+        num_city_b = 0
+        min_sum = 0
+        for cost in costs:
+            if cost[0] < cost[1] and num_city_a < len(costs)//2:
+                min_sum += cost[0]
+                num_city_a += 1
+            elif cost[0] > cost[1] and num_city_b < len(costs)//2:
+                min_sum += cost[1]
+                num_city_b += 1
+            elif num_city_b >= len(costs)//2:
+                min_sum += cost[0]
+                num_city_a += 1
+            else:
+                min_sum += cost[1]
+                num_city_b += 1
+        return min_sum
