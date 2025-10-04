2c505a3c158af2d083a8cf81139af4c3e228f567
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-04 17:35:04 +0300

    Time: 3 ms (72.26%), Space: 18 MB (35.3%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,55 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        temp1 = l1
-        temp2 = l2
-        num1 = []
-        num2 = []
-        while temp1 and temp2:
-            num1.append(temp1.val)
-            num2.append(temp2.val)
-            temp1 = temp1.next
-            temp2 = temp2.next
-        while temp1:
-            num1.append(temp1.val)
-            temp1 = temp1.next
-        while temp2:
-            num2.append(temp2.val)
-            temp2 = temp2.next
-        j = 0
-        temp = 0
-        ans = []
-        while j < len(num1) and j < len(num2):
-            ans.append((num1[j] + num2[j] + temp)%10)
-            maybe = temp
-            temp = 0
-            if num1[j] + num2[j] + maybe> 9:
-                temp += (num1[j] + num2[j] + maybe)//10
-            j += 1
-        while j < len(num1):
-            ans.append((num1[j] + temp)%10)
-            maybe = temp
-            temp = 0
-            if num1[j] + maybe> 9:
-                temp += (num1[j] + maybe)//10
-            j += 1
-        while j < len(num2):
-            ans.append((num2[j] + temp)%10)
-            maybe = temp
-            temp = 0
-            if num2[j] + maybe > 9:
-                temp += (num2[j] + maybe)//10
-            j += 1
-        if temp != 0:
-            ans.append(temp)
-        print(ans)
-        dummy = ListNode()
-        current = dummy
-        for i in ans:
-            new_node = ListNode()
-            new_node.val = i
-            current.next = new_node
-            current = current.next
-        return dummy.next
-            
+        noder = ListNode()
+        carry = 0
+        node = noder
+        while l1 and l2:
+            num = l1.val + l2.val + carry
+            node.next = ListNode(num%10)
+            node = node.next
+            carry = num // 10
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            num = l1.val + carry
+            node.next = ListNode(num%10)
+            node = node.next
+            carry = num // 10
+            l1 = l1.next
+        while l2:
+            num = l2.val + carry
+            node.next = ListNode(num%10)
+            node = node.next
+            carry = num // 10
+            l2 = l2.next
+        while carry:
+            node.next = ListNode(carry%10)
+            node = node.next
+            carry //= 10
+        return noder.next
