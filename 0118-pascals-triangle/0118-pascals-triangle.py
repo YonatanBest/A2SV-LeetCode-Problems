e3a4bc045decb3cb6c8f2fe08a1f005644a53af9
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-05 12:07:32 +0300

    Time: 0 ms (100%), Space: 17.7 MB (59.52%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,17 +1,13 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        ans = [[1]]
         if numRows == 1:
-            return [[1]]
-        ans = [[1], [1, 1]]
-        start = [1, 1]
-        def pascal(test):
-            new = [1,1]
-            x = 0
-            for i in range(1, len(test)):
-                new.insert(1 + x, test[i - 1] + test[i])
-                x += 1
-            ans.append(new)
-        for i in range(numRows - 2):
-            test = ans[-1][:]
-            pascal(test)
+            return ans
+        ans.append([1, 1])
+        while len(ans) < numRows:
+            temp = [1]
+            for j in range(1, len(ans[-1])):
+                temp.append(ans[-1][j] + ans[-1][j-1])
+            temp.append(1)
+            ans.append(temp.copy())
         return ans
