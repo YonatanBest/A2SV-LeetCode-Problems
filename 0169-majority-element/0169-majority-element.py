dcfe651b537658273a55e101d5c2c332df78a133
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-06 19:55:18 +0300

    Time: 0 ms (100%), Space: 19.2 MB (86.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority = defaultdict(int)
-
-        for num in nums:
-            majority[num] += 1
-            if majority[num] > len(nums)/2:
-                return num
+        nums.sort()
+        return nums[len(nums)//2]
