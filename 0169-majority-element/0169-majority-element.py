57c2b6e4978e1e5ac24c479c1753b5ab62fdca2b
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-06 19:46:38 +0300

    Time: 16 ms (8.39%), Space: 19.3 MB (50.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        temp = [nums[0], 1]
         majority = defaultdict(int)
+        
         for num in nums:
             majority[num] += 1
+            if temp[1] < majority[num]:
+                temp = [num, majority[num]]
 
-        ans = sorted(majority.items(), key= lambda x: x[1], reverse=True)
-        return ans[0][0]
+        return temp[0]
