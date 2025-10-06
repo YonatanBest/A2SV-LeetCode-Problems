71a1b7ef3c75f622f1a84d15ef098c06827e3b8c
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-06 20:02:42 +0300

    Time: 11 ms (28.31%), Space: 19.4 MB (50.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        vote = [nums[0], 1]
+        for j in range(1, len(nums)):
+            
+            if vote[0] == nums[j]:
+                vote[1] += 1
+            else:
+                vote[1] -= 1
+                if vote[1] == 0:
+                    vote = [nums[j], 1]
+
+        return vote[0]
