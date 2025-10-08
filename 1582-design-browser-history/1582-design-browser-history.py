02473dd1c0d14d29b03a406a77ad947ec9a4a21a
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-08 14:14:08 +0300

    Time: 47 ms (42.07%), Space: 20.4 MB (18.22%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -1,52 +1,31 @@
 class Node:
-    def __init__(self, id=0, domain="homepage", next=None):
-        self.id = id
-        self.domain = domain
+    def __init__(self, val=0, next=None, prev=None):
+        self.val = val
         self.next = next
-    def __str__(self):
-        return f"{self.id} {self.domain} {self.next}"
+        self.prev = prev
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.dummy = Node(0, homepage)
-        self.dic = {0:self.dummy}
-        self.count = 0
-        self.tail = self.dummy
-        self.current = self.dummy
-        print(self.dummy)
+        self.browser = Node(val=homepage)
+
     def visit(self, url: str) -> None:
-        self.count += 1
-        temp = self.current.id
+        self.browser.next = Node(val=url, prev=self.browser)
+        self.browser = self.browser.next
 
-        for i in range(temp + 2, self.count):
-            del self.dic[i]
-
-        self.count = temp + 1
-        new_node = Node(self.count, url)
-        self.tail = self.current
-        self.tail.next = new_node
-        self.tail = new_node
-        
-        self.current.next = new_node
-        self.current = new_node
-        self.dic[self.count] = new_node
-        print(self.dummy)
     def back(self, steps: int) -> str:
-        pos = self.current.id
-        while pos > 0 and steps > 0:
-            pos -= 1
+        while self.browser.prev and steps:
+            self.browser = self.browser.prev
             steps -= 1
-        self.current = self.dic[pos]
-        print(self.dummy)
-        return self.current.domain
+        return self.browser.val
+
     def forward(self, steps: int) -> str:
-        pos = self.current.id
-        while pos < self.count and steps > 0:
-            pos += 1
+        while self.browser.next and steps:
+            self.browser = self.browser.next
             steps -= 1
-        self.current = self.dic[pos]
-        print(self.dummy)
-        return self.current.domain
+        return self.browser.val
+
+
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
