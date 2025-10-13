2706a02782f6d57c05b91692e839e387ec94bed7
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-13 13:32:22 +0300

    Time: 343 ms (43.13%), Space: 17.9 MB (58.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,39 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        max_str = [s[0]]  
-        for x in range(1, len(s) - 1):
+        max_str = [0, 0]  
+        for x in range(0, len(s) - 1):
             mid = x
             i = mid - 1
             j = mid + 1
-            string = deque([s[mid]])
+            string = [x, x]
             while i >= 0 and j < len(s):
                 if s[i] != s[j]:
                     break
-                string.appendleft(s[i])
-                string.append(s[j])
+                max_str = [i, j] if max_str[1] - max_str[0] < j - i else max_str
                 i -= 1
                 j += 1
 
-            if len(max_str) < len(string):
-                max_str = string.copy()
-
-        for x in range(0, len(s) - 1):
             if x + 1 >= len(s) or s[x] != s[x + 1]:
                 continue
             i = x
             j = x + 1
-            string = deque([s[i], s[j]])
+            max_str = [i, j] if max_str[1] - max_str[0] < j - i else max_str
             i -= 1
             j += 1
             while i >= 0 and j < len(s):
                 if s[i] != s[j]:
                     break
-                string.appendleft(s[i])
-                string.append(s[j])
+                max_str = [i, j] if max_str[1] - max_str[0] < j - i else max_str
                 i -= 1
                 j += 1
-
-            if len(max_str) < len(string):
-                max_str = string.copy()
-
-        return "".join(max_str)
+        return s[max_str[0]:max_str[1] + 1]
