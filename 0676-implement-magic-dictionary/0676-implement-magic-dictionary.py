07f36dac2bde38f6e4bbcc8c4c5ef8f24ac2d45f
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-16 16:56:18 +0300

    Time: 113 ms (50.32%), Space: 20.8 MB (33.67%) - LeetHub

---
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self):
+        self.flag = False
+        self.children = {}
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.Trie = Node()
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        for word in dictionary:
+            temp = self.Trie
+            for char in word:
+                if char not in temp.children:
+                    temp.children[char] = Node()
+                temp = temp.children[char]
+            temp.flag = True
+
+
+    def search(self, searchWord: str) -> bool:
+        return self._search(searchWord, False, 0, self.Trie)
+
+    def _search(self, word, check, i, temp):
+        if i == len(word):
+            if temp.flag and check:
+                return True
+            return False
+        if word == "judge":
+            print(temp.children, check, i)
+        for j in temp.children:
+            
+            if check and word[i] != j:
+                continue
+
+            if word[i] != j:
+                if self._search(word, True, i + 1, temp.children[j]):
+                    return True
+            elif self._search(word, check, i + 1, temp.children[j]):
+                return True
+       
+        return False
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
