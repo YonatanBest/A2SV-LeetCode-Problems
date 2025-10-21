59f280e8a8c804b033d185a0e03f0a746366f9e6
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2025-10-21 14:32:59 +0300

    Time: 1060 ms (5.03%), Space: 18.3 MB (91.48%) - LeetHub

--- 1272-invalid-transactions/1272-invalid-transactions.py
+++ 1272-invalid-transactions/1272-invalid-transactions.py
@@ -1,31 +1,20 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        already_done = set()
-        invalid_transactions = []
+        invalid_transactions = set()
         for i in range(len(transactions)):
-            for j in range(len(transactions)):
-
-                if i == j:
-                    continue
-
+            for j in range(i + 1, len(transactions)):
                 name_i, time_i, amount_i, city_i = transactions[i].split(",")
                 name_j, time_j, amount_j, city_j = transactions[j].split(",")
 
                 if name_i == name_j and abs(int(time_j) - int(time_i)) <= 60 and city_i != city_j:
-                    if i not in already_done:
-                        invalid_transactions.append(transactions[i])
-                        already_done.add(i)
-                    if j not in already_done:
-                        invalid_transactions.append(transactions[j])
-                        already_done.add(j)
+                    invalid_transactions.add(i)
+                    invalid_transactions.add(j)
                     continue
                 
-                if int(amount_i) > 1000 and i not in already_done:
-                    invalid_transactions.append(transactions[i])
-                    already_done.add(i)
+                if int(amount_i) > 1000:
+                    invalid_transactions.add(i)
 
-                if int(amount_j) > 1000 and j not in already_done:
-                    invalid_transactions.append(transactions[j])
-                    already_done.add(j)
+                if int(amount_j) > 1000:
+                    invalid_transactions.add(j)
 
-        return invalid_transactions
+        return [transactions[i] for i in invalid_transactions]
